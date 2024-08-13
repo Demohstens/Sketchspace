91d49500c49cb286680adff415ca755ccbdb222b
demohstens <demohstens@gmail.com>
2024-08-13 02:18:13 -0300

    Added zoom widget custom impl

--- lib/canvas/canvas_viewport.dart
+++ lib/canvas/canvas_viewport.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:sketchspace/brushes/active_painter.dart';
+import 'package:sketchspace/brushes/lazy_painter.dart';
 import 'package:sketchspace/canvas/canvas_context.dart';
 import 'package:provider/provider.dart';
 import 'package:sketchspace/canvas/data/scale.dart';
+import 'package:sketchspace/canvas/data/worldspace.dart';
+import 'package:sketchspace/canvas/zoom-widget/lib/zoom_widget.dart';
+import 'package:sketchspace/classes/settings.dart';
 
 class CanvasViewport extends StatelessWidget {
   @override
@@ -12,42 +16,64 @@ class CanvasViewport extends StatelessWidget {
       children: [
         Consumer<ScaleProvier>(builder: (context, scaleContext, _) {
           return Container(
+              color: context.watch<Settings>().background,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
               constraints: BoxConstraints(
                   minWidth: MediaQuery.of(context).size.width,
                   minHeight: MediaQuery.of(context).size.height),
-              // Gesture handling for the Canvas
-              child: GestureDetector(
-                onDoubleTap: () {
-                  context.read<DrawingContext>().toggleUI();
-                },
-                // onLongPressStart: (details) {
-                //   context
-                //       .read<DrawingContext>()
-                //       .selectStroke(details.globalPosition);
-                // },
-                onScaleStart: (details) {
-                  if (details.pointerCount > 1) {
-                    scaleContext.startScaling(details);
-                  } else {
-                    scaleContext.endScaling();
-                  }
-                },
-                onScaleUpdate: (details) {
-                  if (details.pointerCount > 1) {
-                    scaleContext.updateScale(details);
-                  } else {
+              child: Zoom(
+                canvasColor: context.watch<Settings>().background,
+                doubleTapZoom: false,
+                maxScale: 3,
+                maxZoomWidth: 2000,
+                maxZoomHeight: 2000,
+                onScaleUpdateDetails: (details) {
+                  if (details.pointerCount == 1) {
                     context
                         .read<DrawingContext>()
                         .updateDrawing((details.localFocalPoint));
                   }
                 },
-                onScaleEnd: (details) {
-                  scaleContext.endScaling();
-                  context.read<DrawingContext>().endDrawing();
-                },
+                child:
+                    //  GestureDetector(
+                    //   onDoubleTap: () {
+                    //     context.read<DrawingContext>().toggleUI();
+                    //   },
+                    //   // onLongPressStart: (details) {
+                    //   //   context
+                    //   //       .read<DrawingContext>()
+                    //   //       .selectStroke(details.globalPosition);
+                    //   // },
+                    //   // onScaleStart: (details) {
+                    //   //   if (details.pointerCount > 1) {
+                    //   //     scaleContext.startScaling(details);
+                    //   //   } else {
+                    //   //     scaleContext.endScaling();
+                    //   //   }
+                    //   // },
+                    //   onPanUpdate: (details) {
+                    //     context
+                    //         .read<DrawingContext>()
+                    //         .updateDrawing((details.localPosition));
+                    //   },
+                    //   // onScaleEnd: (details) {
+                    //   //   scaleContext.endScaling();
+                    //   //   context.read<DrawingContext>().endDrawing();
+                    //   // },
 
-                // The Visual Representation of the Canvas
-                child: Stack(children: [
+                    //   // The Visual Representation of the Canvas
+                    //   child:
+                    Stack(children: [
+                  Positioned.fill(
+                      child: RepaintBoundary(
+                    child: CustomPaint(
+                        willChange: false,
+                        isComplex: true,
+                        size: Size.infinite,
+                        painter: LazyPainter(context.read<Worldspace>().strokes,
+                            context.read<DrawingContext>().repaintListener)),
+                  )),
                   // Current Path - CurrentLinePainter
                   Container(
                     width: MediaQuery.of(context).size.width,
--- lib/pages/canvas.dart
+++ lib/pages/canvas.dart
@@ -1,41 +1,23 @@
-import 'package:sketchspace/brushes/lazy_painter.dart';
 import 'package:sketchspace/canvas/canvas_viewport.dart';
-import 'package:sketchspace/canvas/data/worldspace.dart';
 import 'package:sketchspace/canvas/canvas_context.dart';
-import 'package:sketchspace/canvas/data/scale.dart';
-import 'package:sketchspace/classes/settings.dart';
 import 'package:sketchspace/canvas/canvasUI.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sketchspace/canvas/data/stroke_selector/src/stroke.dart';
-import 'package:zoom_widget/zoom_widget.dart';
 
 class CanvasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<Stroke> buffer =
-        context.select<Worldspace, List<Stroke>>((value) => value.strokes);
     return Container(
-        color: context.watch<Settings>().background,
         child: Stack(
-          children: [
-            Positioned.fill(
-                child: RepaintBoundary(
-              child: CustomPaint(
-                  willChange: false,
-                  isComplex: true,
-                  size: Size.infinite,
-                  painter: LazyPainter(
-                      buffer, context.read<DrawingContext>().repaintListener)),
-            )),
-            Positioned.fill(
-              child: CanvasViewport(),
-            ),
-            Visibility(
-              visible: context.watch<DrawingContext>().ui_enabled,
-              child: CanvasUI(),
-            ),
-          ],
-        ));
+      children: [
+        Positioned.fill(
+          child: CanvasViewport(),
+        ),
+        Visibility(
+          visible: context.watch<DrawingContext>().ui_enabled,
+          child: CanvasUI(),
+        ),
+      ],
+    ));
   }
 }
